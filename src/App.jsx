cae7533299949df6954805c598d1d316157fb927
Job Chow <jobitoalv@gmail.com>
2023-03-18 18:29:04 +1000

    create folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
-import  { BrowserRouter } from 'react-router-dom';
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components';
+import { BrowserRouter } from "react-router-dom";
 
-
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 
 const App = () => {
   return (
@@ -25,5 +24,4 @@ const App = () => {
   );
 }
 
-
-export default App
+export default App;
