dd1da9b09476a94747b3ef61159daf3f62b7ebcb
Job Chow <jobitoalv@gmail.com>
2023-03-18 14:52:40 +1000

    create sections on app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
-const App = () =>{
+import  { BrowserRouter } from 'react-router-dom';
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components';
+
+
+
+const App = () => {
 
   return (
-    <div>
-      3 Developer Portfolio 
-    </div>
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+      </div>
+      <About />
+      <Experience />
+      <Tech />
+      <Works />
+      <Feedbacks />
+      <div className="relative z-0">
+
+      </div>
+    </BrowserRouter>
   )
 }
 
