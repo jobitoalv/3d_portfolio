94d70498ec94226d26c53c5893d933f7a82b7653
Job Chow <jobitoalv@gmail.com>
2023-03-19 19:03:08 +1000

    3d image roation complete

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -12,6 +12,9 @@ const Computers = () => {
      <pointLight intensity={1}/>
      <primitive
        object={computer.scene}
+       scale={0.75}
+       position={[0, -3.25, -1.5]}
+       rotation={[-0.01, -0.2, -0.1]}
      />
     </mesh>
   )
