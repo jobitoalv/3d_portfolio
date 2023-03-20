36cbcb415a8cec370181d83cd0e8f9073a4ca2ed
Job Chow <jobitoalv@gmail.com>
2023-03-20 13:50:12 +1000

    create wrapper file to wrapper style all page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,6 +3,7 @@ import Tilt from 'react-tilt'
 import { motion } from 'framer-motion';
 import { styles } from '../styles';
 import { services } from '../constants';
+import { SectionWrapper } from "../hoc";
 import { fadeIn, textVariant } from '../utils/motion';
 
 const ServiceCard = ({ index, title, icon }) => {
@@ -18,8 +19,11 @@ const ServiceCard = ({ index, title, icon }) => {
             scale: 1,
             speed: 450
           }}
+          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
         >
-
+          <img src={icon} alt={title}
+            className="w-16 h-16 object-contain"/>
+          <h3 className="text-white text-[20px] font-bold text-center">{title}</h3>
         </div>
       </motion.div>
     </Tilt>
@@ -31,7 +35,7 @@ const About = () => {
     <>
       <motion.div variants={textVariant()}>
         <p className={styles.sectionSubtext}>Introduction</p>
-        <h2 className={styles.sectionHeadText}>OverView.</h2>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
       </motion.div>
 
       <motion.p variants={fadeIn("","", 0.1, 1)}
@@ -50,4 +54,4 @@ const About = () => {
   )
 }
 
-export default About
+export default SectionWrapper(About, "about");
--- src/constants/index.js
+++ src/constants/index.js
@@ -42,7 +42,7 @@ export const navLinks = [
 
 const services = [
   {
-    title: "Web Developer",
+    title: "Software Developer",
     icon: web,
   },
   {
